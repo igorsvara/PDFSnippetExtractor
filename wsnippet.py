21410674badd7ff4c489c1eef93ac3cb6abd6a7e
igor <svaraigorw@gmail.com>
2024-02-29 19:40:28 +0100

    finalizzato il programma per la ricerca di single parole, non regex, aggiunto todo

--- wsnippet.py
+++ wsnippet.py
@@ -12,13 +12,12 @@ new_doc = fitz.open()
 
 # y e' indice delle pagine all'interno del nuovo file
 y = 0
-for x in range(doc.page_count):
-    page = doc[x]
-
+for page in doc:
     curr_page_number = page.number
     curr_page_width = page.rect.width
     curr_page_height = page.rect.height
 
+    # TODO: trova un modo per usare regex come search pattern
     occurrences = page.search_for(parola_cercata)
     if not occurrences:
         continue
@@ -34,6 +33,7 @@ for x in range(doc.page_count):
         new_doc[y].set_cropbox(cropping)
         # incremento y ogni volta che inserisco una pagina nel nuovo pdf
         y += 1
+        print(f"page {x} -> match #{i}")
 
 if new_doc.page_count:
     new_doc.save(new_doc_name)

6d4db789c5ab9ecc43f8bb70e0c6fe3813aadcd6
igor <svaraigorw@gmail.com>
2024-02-29 18:46:08 +0100

    programma funzionante

--- wsnippet.py
+++ wsnippet.py
@@ -1,15 +1,19 @@
 import fitz
 
-doc = fitz.open("DB_2023-01-27.pdf")
-new_doc_name = "nuovofile.pdf"
-snippet_line_number = 2
-parola_cercata = "base di dati"
+from_pdf = "dispenseLM2122.pdf"
+parola_cercata = "Definizione"
 
+new_doc_name = "result.pdf"
+snippet_line_number = 6
+line_margin = 3
 
-doc_page_number = doc.page_count
-relative_shift = 0
-for x in range(doc_page_number):
-    page = doc[x + relative_shift]
+doc = fitz.open(from_pdf)
+new_doc = fitz.open()
+
+# y e' indice delle pagine all'interno del nuovo file
+y = 0
+for x in range(doc.page_count):
+    page = doc[x]
 
     curr_page_number = page.number
     curr_page_width = page.rect.width
@@ -19,24 +23,19 @@ for x in range(doc_page_number):
     if not occurrences:
         continue
 
-    i = 0
-    for occ in occurrences:
-        line_margin = 3
+    for i, occ in enumerate(occurrences):
         line_height = occ[3] - occ[1] + line_margin
         y_end_crop = occ[1] + line_height * snippet_line_number
         if y_end_crop > curr_page_height:
             y_end_crop = curr_page_height
         cropping = fitz.Rect(0, occ[1], curr_page_width, y_end_crop)
 
-        # Inserisco la copia subito dopo alla posizione indicata con curr_page_number  + i
-        doc.fullcopy_page(curr_page_number, curr_page_number + i)
-        # Ritaglio la pagina appena inserita
-        print(f"{curr_page_number+i+1} page -> crop {cropping}")
-        doc[curr_page_number + i + 1].set_cropbox(cropping)
-        i += 1
+        new_doc.insert_pdf(doc, from_page=curr_page_number, to_page=curr_page_number)
+        new_doc[y].set_cropbox(cropping)
+        # incremento y ogni volta che inserisco una pagina nel nuovo pdf
+        y += 1
 
-    relative_shift += i
-    # doc.delete_page(curr_page_number)
-
-doc.save(new_doc_name)
+if new_doc.page_count:
+    new_doc.save(new_doc_name)
+new_doc.close()
 doc.close()

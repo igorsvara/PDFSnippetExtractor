8a3c95f48ca922f439f7898b729382837c91ad27
igor <svaraigorw@gmail.com>
2024-02-29 15:43:54 +0100

    working program for single page extraction - improved

--- wsnippet.py
+++ wsnippet.py
@@ -1,6 +1,8 @@
 import fitz
 
 doc = fitz.open("singlepage.pdf")
+new_doc_name = "nuovofile.pdf"
+snippet_line_number = 2
 parola_cercata = "esercizi"
 
 page = doc[0]
@@ -18,10 +20,10 @@ for occ in occurrences:
     i += 1
     line_margin = 3
     line_height = occ[3] - occ[1] + line_margin
-    cropping = fitz.Rect(0, occ[1], curr_page_width, occ[1] + line_height * 2)
+    cropping = fitz.Rect(0, occ[1], curr_page_width, occ[1] + line_height * snippet_line_number)
 
     doc.fullcopy_page(curr_page_number)
     cropped_page = doc[curr_page_number + i].set_cropbox(cropping)
 
-doc.save("nuovofile.pdf")
+doc.save(new_doc_name)
 doc.close()
